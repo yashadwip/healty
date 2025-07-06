ff591229037a89935fa594997939778bdb188ed6
yashadwip <ydwiprianti@gmail.com>
2025-07-07 00:54:00 +0800

    Clean project and add .gitignore

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_ckeditor_5.fields',
+    'django_ckeditor_5',
     'artikel',
     'galeri',
 ]
